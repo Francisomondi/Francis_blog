b8642ad558711310d65d1c1d1cc7cd3628a7216a
francis omondi <francisomondi17@gmail.com>
2022-05-12 23:37:49 +0300

    handling click event

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 const Home = () => {
+  const handleClick = () => {
+    console.log("button clicked");
+  };
+
+  const handleClickAgain = name => {
+    console.log("hello " + name);
+  };
+
   return (
     <div className="home">
       <h2>This is going to be the body</h2>
+      <button onClick={handleClick}>click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("Francis");
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 };
