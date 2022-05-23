34ea3c0b5c6519678a2eebfd6c9360421924c2d4
francis omondi <francisomondi17@gmail.com>
2022-05-23 07:01:32 +0300

    conrolled inputs- getting user inputs

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Francis Omondi ");
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
       <form>
         <label>Blog Title</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
         <label>Blog Body</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={e => setBody(e.target.value)}
+        ></textarea>
         <label>Blog Author</label>
-        <select>
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
           <option value="Francis Omondi">Francis Omondi</option>
           <option value="Brian Otieno">Brian Otieno</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
   );
