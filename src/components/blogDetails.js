be6d0085b1d0000a982a7a0a571cd1fab9e710c5
francis omondi <francisomondi17@gmail.com>
2022-05-23 05:48:39 +0300

    reusing a custom hook to fetch data from json serer- making a fetch effect

--- src/components/blogDetails.js
+++ src/components/blogDetails.js
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch(
+    "http://localhost:5000/blogs/" + id
+  );
   return (
-    <div className="blog-blogDetails">
-      <h2>blog details - {id}</h2>
+    <div className="blog-details">
+      {isPending && <div>Loading ...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <aticle>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </aticle>
+      )}
     </div>
   );
 };
