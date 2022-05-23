dcc29b61e3898d74b04999c54522ffd0e402abe5
francis omondi <francisomondi17@gmail.com>
2022-05-23 06:19:46 +0300

    web form

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,6 +2,17 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog Title</label>
+        <input type="text" required />
+        <label>Blog Body</label>
+        <textarea required></textarea>
+        <label>Blog Author</label>
+        <select>
+          <option value="Francis Omondi">Francis Omondi</option>
+          <option value="Brian Otieno">Brian Otieno</option>
+        </select>
+      </form>
     </div>
   );
 };
